90132982ab103072e6d45a3a452d4ca839980901
Alemayehu Girma <alemayehu.girma@hotmail.com>
2022-06-06 17:29:08 +0200

    reimplemented input field

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -12,22 +12,35 @@ const Calendar = () => {
 			id="calendar"
 			className=" bg-black bg-opacity-50 backdrop-blur-3xl rounded-md mt-2 w-1/3 cursor-default shadow-lg"
 		>
-			<input
-				className="text-center font-semibold text-slate-200 w-full appearance-none focus:outline-none rounded-t-md bg-green-600 bg-opacity-50 cursor-default"
-				value={year}
-				placeholder={year}
-				onChange={(e) => {
-					setYear(e.target.value);
-				}}
-				onBlur={(e) => {
-					setYear(e.target.value);
-				}}
+			{/* changed input field to div with button to remove rendering on every key stoke and improve performance */}
+			<div
+				className="text-center font-semibold text-white w-full appearance-none focus:outline-none rounded-t-md bg-blue-600 bg-opacity-50 cursor-default"
 				onClick={resetCalendarLayoutClickedInput}
-			/>
+			>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py px-4 rounded m-1 mr-6"
+					onClick={() => setYear(year - 1)}
+				>
+					-
+				</button>
+				{year}
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py px-4 rounded m-1 ml-6"
+					onClick={() => setYear(year + 1)}
+				>
+					+
+				</button>
+			</div>
 			<div className=" grid grid-cols-3 grid-rows-4 gap-2 p-2 ">
 				{daysInMonth.map((val, ind) => {
 					return (
-						<Calendarmonthcontainer key={ind * val} month={ind + 1} year={year} maxDay={val} />
+						<Calendarmonthcontainer
+							key={ind * val}
+							month={ind + 1}
+							year={year}
+							maxDay={val}
+							onChange={console.log("month")}
+						/>
 					);
 				})}
 				<Todo />
