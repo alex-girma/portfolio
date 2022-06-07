4d8e5539e846ffdba95086af0a0f59cbd3d1e8cc
Alemayehu Girma <alemayehu.girma@hotmail.com>
2022-06-07 10:12:38 +0200

    todo

--- src/pages/Todo/Todo.js
+++ src/pages/Todo/Todo.js
@@ -5,11 +5,6 @@ import NewToDo from "./NewToDo";
 import ToDoList from "./ToDoList";
 
 const Todo = ({ selectedDate, today }) => {
-	const [toDos, setToDos] = useState([
-		{ val: "Test1", done: false },
-		{ val: "Test2", done: false },
-		{ val: "Test3", done: false },
-	]);
 	const allToDos = {
 		"1.1.2022": [
 			{ val: "Test1", done: false },
@@ -22,6 +17,13 @@ const Todo = ({ selectedDate, today }) => {
 			{ val: "Test6", done: false },
 		],
 	};
+	const [toDos, setToDos] = useState(
+		[
+			{ val: "Test1", done: false },
+			{ val: "Test2", done: false },
+			{ val: "Test3", done: false },
+		] || []
+	);
 
 	return (
 		<div id="todo" className="hidden pt-4 rounded-sm border border-slate-400 pl-2 pr-2">
