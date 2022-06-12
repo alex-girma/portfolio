9d8d34349d5a86c9d72df6be47ebabf2c20fe57d
Alemayehu Girma <alemayehu.girma@hotmail.com>
2022-06-12 23:23:14 +0200

    refactoring

--- src/pages/Topbar/Weatherbar.js
+++ src/pages/Topbar/Weatherbar.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { toggleHiddenWindows } from "../../utility/functions";
 
 const Weatherbar = () => {
+	// TODO: fetching weather twice for Weatherbar.js and Weather.js change it
 	const [weather, setWeather] = useState({});
 	const [fetchday, setFetchDay] = useState(new Date().getDate());
 	const [isLoading, setIsLoading] = useState(true);
--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -1,9 +1,47 @@
+import { useEffect, useState } from "react";
 import WeatherDays from "./WeatherDays";
 
 const Weather = () => {
-	const forCast = JSON.parse(localStorage.getItem("fetchedWeather")) || {};
+	// TODO: fetching weather twice for Weatherbar.js and Weather.js change it
+	//const forCast = JSON.parse(localStorage.getItem("fetchedWeather")) || {};
+	const [weather, setWeather] = useState({});
+	const [fetchday, setFetchDay] = useState(new Date().getDate());
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		if (fetchday < new Date().getDate()) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				const lat = position.coords.latitude;
+				const long = position.coords.longitude;
+				requestWeather(lat, long);
+				setFetchDay(new Date().getDate());
+			});
+		}
+		if (localStorage.getItem("fetchedWeather")) {
+			setIsLoading(false);
+			return setWeather(JSON.parse(localStorage.getItem("fetchedWeather")));
+		}
+		navigator.geolocation.getCurrentPosition((position) => {
+			const lat = position.coords.latitude;
+			const long = position.coords.longitude;
+			requestWeather(lat, long);
+			setFetchDay(new Date().getDate());
+		});
 
-	if (Object.keys(forCast).length === 0) return <div id="weather__window"></div>;
+		async function requestWeather(lat, long) {
+			try {
+				const url = `https://weatherdbi.herokuapp.com/data/weather/${lat},${long}`;
+				const res = await fetch(`${url}`);
+				const json = await res.json();
+				setWeather(json);
+				setIsLoading(false);
+				localStorage.setItem("fetchedWeather", JSON.stringify(json));
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}, [fetchday]);
+
+	if (isLoading) return <div id="weather__window"></div>;
 	return (
 		<div
 			id="weather__window"
@@ -13,20 +51,20 @@ const Weather = () => {
 				<div>
 					<img
 						className="w-30 h-30 "
-						alt={forCast.currentConditions.comment}
-						src={forCast.currentConditions.iconURL}
+						alt={weather.currentConditions.comment}
+						src={weather.currentConditions.iconURL}
 					/>
 				</div>
 				<p className="text-slate-200 font-semibold text-6xl px-8 py-4">
-					{forCast.currentConditions.temp.c}°
+					{weather.currentConditions.temp.c}°
 				</p>
 				<div className="flex flex-col text-slate-200 font-semibold">
-					<p>{forCast.currentConditions.comment}</p>
-					<p>{forCast.region.split(", ").pop()}</p>
+					<p>{weather.currentConditions.comment}</p>
+					<p>{weather.region.split(", ").pop()}</p>
 				</div>
 			</div>
 			<div className="flex place-content-between mb-3">
-				{forCast.next_days.slice(1, 8).map((val, ind) => (
+				{weather.next_days.slice(1, 8).map((val, ind) => (
 					<WeatherDays
 						key={val.max_temp + val.day + ind}
 						day={val.day}
