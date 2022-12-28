663b167f883411802533215f3b22f6ec609ed6f4
Alemayehu Girma <alemayehu.girma@hotmail.com>
2022-12-28 14:38:43 +0100

    refactor clock

--- components/clock/ClockApp.tsx
+++ components/clock/ClockApp.tsx
@@ -4,24 +4,20 @@ import { toIntlDateFormat } from '../utility/functions';
 
 const ClockApp = () => {
   const [locale, setLocale] = useState<string>('en-US');
-  const [secondDeg, setSecondDeg] = useState(0);
-  const [minuteDeg, setMinuteDeg] = useState(0);
-  const [hourDeg, setHourDeg] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [hour, setHour] = useState(0);
   useEffect(() => {
     const timer = setTimeout(() => {
-      const now = new Date();
-      setSecondDeg((now.getSeconds() + 1) * 6);
-      setMinuteDeg(now.getMinutes() * 6);
-      setHourDeg(
-        now.getHours() * 30 +
-          ((30 / 60) * now.getMinutes() + (30 / 60 / 60) * now.getSeconds())
-      );
+      setSecond(new Date().getSeconds());
+      setMinute(new Date().getMinutes());
+      setHour(new Date().getHours());
     }, 1000);
     return () => clearTimeout(timer);
-  }, [secondDeg]);
+  }, [second]);
   useEffect(() => {
     setLocale(navigator.language);
-  }, []);
+  }, [locale]);
   return (
     <AppWindowWrapper>
       <div className="flex flex-col items-center gap-6 p-5">
@@ -31,18 +27,22 @@ const ClockApp = () => {
               {toIntlDateFormat(locale, new Date())}
             </div>
             <div
-              style={{ transform: `rotate(${hourDeg}deg)` }}
+              style={{
+                transform: `rotate(${
+                  hour * 30 + ((30 / 60) * minute + (30 / 60 / 60) * second)
+                }deg)`,
+              }}
               className="w-1.5 h-16 bg-orange-500 position-hands shadow-hour z-20"
             ></div>
             <div
-              style={{ transform: `rotate(${minuteDeg}deg)` }}
-              className="w-1.5 h-20 bg-blue-500 position-hands shadow-min z-30 duration-200 ease-out"
+              style={{ transform: `rotate(${minute * 6}deg)` }}
+              className="w-1.5 h-20 bg-blue-500 position-hands shadow-min z-30 ease-out"
             ></div>
             <div
-              style={{ transform: ` rotate(${secondDeg}deg)` }}
-              className="w-1 h-28 bg-red-500 position-hands shadow-sec z-40 duration-100 ease-out"
+              style={{ transform: ` rotate(${second * 6}deg)` }}
+              className="w-1 h-28 bg-red-500 position-hands shadow-sec z-40 ease-out"
             ></div>
-            <div className="w-3 h-3 bg-slate-600 absolute-l-t translate-center rounded-full shadow z-50"></div>
+            <div className="w-3 h-3 bg-slate-600 absolute-l-t -translate-x-1/2 -translate-y-1/2 rounded-full shadow z-50"></div>
 
             <div className="absolute left-1/2 top-5 position-hours">12</div>
             <div className="absolute right-3 top-1/2 position-hours">3</div>
@@ -75,7 +75,20 @@ const ClockApp = () => {
             </div>
           </div>
         </div>
-        <div className="">Time</div>
+        <div className="flex gap-2 bg-slate-400 p-2 text-white">
+          <p>
+            {new Date()
+              .toLocaleTimeString(locale, { hour: '2-digit', hour12: true })
+              .slice(0, 2)}
+          </p>
+          <p>{new Date().toLocaleTimeString(locale, { minute: '2-digit' })}</p>
+          <p>
+            {new Date().toLocaleTimeString(locale, { hour12: false }).slice(-2)}
+          </p>
+          <p>
+            {new Date().toLocaleTimeString(locale, { hour12: true }).slice(-2)}
+          </p>
+        </div>
         <div className="flex justify-between w-full">
           <div>Alarm</div>
           <div>Time</div>
