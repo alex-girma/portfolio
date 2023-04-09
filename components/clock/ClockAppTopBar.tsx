eff08177b37f0d17bce8c883999526ec9e782f12
Alemayehu Girma <alemayehu.girma@hotmail.com>
2023-04-09 19:12:10 +0200

    comment clock app

--- components/clock/ClockAppTopBar.tsx
+++ components/clock/ClockAppTopBar.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import { toIntlTimeFormat } from '../utility/functions';
 
-const ClockAppTopBar: React.FC = () => {
+const ClockAppTopBar = () => {
   const [time, setTime] = useState('');
 
   useEffect(() => {
-    const locale = navigator.language;
+    const locale = navigator.language || 'en-US';
+    // to avoid showing empty time on screen because of the 1s delay
     if (time === '') setTime(toIntlTimeFormat(locale, new Date()));
     const timer = setTimeout(
       () => setTime(toIntlTimeFormat(locale, new Date())),
