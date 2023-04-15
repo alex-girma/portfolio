7757ef11e9d0ba9ac095931d72f8929763a2aca5
Alemayehu Girma <alemayehu.girma@hotmail.com>
2023-04-15 11:20:32 +0200

    merge sort

--- components/sort/SortHeader.tsx
+++ components/sort/SortHeader.tsx
@@ -139,26 +139,7 @@ const SortHeader = ({
   };
 
   const mergeSort = () => {
-    const sort = (array: number[]): number[] => {
-      const half = array.length / 2;
-      if (array.length < 2) {
-        return array;
-      }
-      const left = array.splice(0, half);
-      return merge(sort(left), sort(array));
-    };
-    const merge = (left: number[], right: number[]): number[] => {
-      let arr: number[] = [];
-      while (left.length && right.length) {
-        // if (left[0] < right[0]) {
-        //   arr.push(left.shift());
-        // } else {
-        //   arr.push(right.shift());
-        // }
-      }
-      return [...arr, ...left, ...right];
-    };
-    sort(dataArray);
+    console.log('Merge');
   };
   const quickSort = async () => {
     console.log('Quick');
