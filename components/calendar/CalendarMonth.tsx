df72e70eb31b2f0015ae2865a81147976106c352
Alemayehu Girma <alemayehu.girma@hotmail.com>
2023-04-17 18:16:21 +0200

    responsive calendar

--- components/calendar/CalendarMonth.tsx
+++ components/calendar/CalendarMonth.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 
 export interface CalendarMonthProps {
   year: number;
@@ -32,18 +32,14 @@ const CalendarMonth = ({
     setMonthIndex(index + 1);
   };
   return (
-    <div className="text-l flex justify-center gap-2 py-2">
-      <button className=" h-6 px-3 text-xs hover:bg-gray-100" onClick={test1}>
+    <div className="text-l mt-4 flex justify-center gap-2 border-t-2 border-orange-400 py-4">
+      <button className=" h-4 px-3 text-xs hover:bg-gray-100" onClick={test1}>
         -
       </button>
       <p>
         {currentMonthName} - {year}
       </p>
-      <button
-        className=" h-6 px-3 text-xs hover:bg-gray-100"
-        // onClick={() => setMonthIndex(index + 1)}
-        onClick={test}
-      >
+      <button className=" h-4 px-3 text-xs hover:bg-gray-100" onClick={test}>
         +
       </button>
     </div>
