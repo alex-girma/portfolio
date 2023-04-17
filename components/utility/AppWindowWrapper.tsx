d358b64a8ad7904391cdc28cea39bfa5e2dd840c
Alemayehu Girma <alemayehu.girma@hotmail.com>
2023-04-17 19:08:42 +0200

    appwrapper responsive

--- components/utility/AppWindowWrapper.tsx
+++ components/utility/AppWindowWrapper.tsx
@@ -9,14 +9,21 @@ const AppWindowWrapper = ({ children }: AppWindowWrapperProps) => {
   const route = useRouter();
   return (
     <div>
-      <div className="flex justify-between">
-        <div>{route.pathname.slice(1)}</div>
+      <div className="flex justify-between rounded-t bg-orange-800 pl-3 text-white ">
+        <div className="flex items-center text-sm">
+          {route.pathname.slice(1).charAt(0).toUpperCase() +
+            route.pathname.slice(2)}
+        </div>
         <div>
           <Link href={'/'}>
-            <button className="h-7 px-4 text-xs hover:bg-gray-100">⚊</button>
+            <button className="h-7 px-4 text-xs duration-300 hover:bg-orange-500">
+              ⚊
+            </button>
           </Link>
           <Link href={'/'}>
-            <button className="h-7 px-4 text-xs hover:bg-red-600">✕</button>
+            <button className="h-7 rounded-tr px-4 text-xs duration-300 hover:bg-red-600 ">
+              ✕
+            </button>
           </Link>
         </div>
       </div>
