aa0998b407838be13ad34c80b98976d052229cfc
Alemayehu Girma <alemayehu.girma@hotmail.com>
2023-04-17 20:19:53 +0200

    finish responsive sort

--- components/sort/SortVisualizer.tsx
+++ components/sort/SortVisualizer.tsx
@@ -4,7 +4,7 @@ const SortVisualizer = ({ number }: { number: number }) => {
       className="rotatex0 flex w-4 items-end justify-center bg-gray-400 text-xxxs text-gray-900 sm:w-6 sm:text-xs"
       id={String(number)}
       style={{
-        height: `${number * 4 + 20}px`,
+        height: `${number * 3 + 14}px`,
       }}
     >
       {number}
