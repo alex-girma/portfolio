7f5d59249fa00886f753617dc73ee5b779822ab5
Alemayehu Girma <alemayehu.girma@hotmail.com>
2023-05-08 14:58:48 +0200

    add linkedin and github link

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,34 @@ const Home: NextPageWithLayout = () => {
       <Head>
         <title>Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
       </Head>
       <h1 className="text-xl font-normal ">
-        Welcome to my
-        <span className="bg-red-500 px-2 py-1 text-white">Portfolio</span>
+        <div className="flex">
+          <div>
+            <br />
+            <span>Welcome to my</span>
+            <span className="bg-red-500 px-2 py-1 text-white">Portfolio</span>
+          </div>
+          <div>
+            <div className="fa fa-linkedin  pl-20 text-red-500"></div>
+            <a
+              className="pl-2 text-sm"
+              href="https://linkedin.com/in/alexgirmaw"
+            >
+              @alexgirmaw
+            </a>
+
+            <div className="invisible">Test</div>
+            <div className=" fa fa-github pl-20 text-red-500 "></div>
+            <a className="px-2  text-sm" href="https://github.com/alex-girma">
+              @alex-girma
+            </a>
+          </div>
+        </div>
       </h1>
     </>
   );
